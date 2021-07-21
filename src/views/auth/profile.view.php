60bdaba220e2fd97940ea4758946103dc9e5c726
Judywen Guapin <37282871+jagwarthegreat@users.noreply.github.com>
2021-07-22 00:14:59 +0800

    initial commit

---
+++ src/views/auth/profile.view.php
@@ -0,0 +1,7 @@
+<?php
+
+require __DIR__ . '/../../../../../../app/views/layouts/head.php';
+
+require __DIR__ . '/../../../../../../app/views/layouts/profile.php';
+
+require __DIR__ . '/../../../../../../app/views/layouts/footer.php';
