f6a846ecf440a63fd087ace057ea4d2d12fbc31c
Judywen Guapin <37282871+jagwarthegreat@users.noreply.github.com>
2021-07-22 00:34:21 +0800

    restructure profile to be modified by user

--- src/views/auth/profile.view.php
+++ src/views/auth/profile.view.php
@@ -1,7 +1,3 @@
 <?php
 
-require __DIR__ . '/../../../../../../app/views/layouts/head.php';
-
 require __DIR__ . '/../../../../../../app/views/layouts/profile.php';
-
-require __DIR__ . '/../../../../../../app/views/layouts/footer.php';
