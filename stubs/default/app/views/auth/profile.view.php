9f3fad4185281998746a9c40fe847942448ea5c4
Judywen Guapin <37282871+jagwarthegreat@users.noreply.github.com>
2022-03-01 15:52:30 +0800

    Update profile.view.php

--- stubs/default/app/views/auth/profile.view.php
+++ stubs/default/app/views/auth/profile.view.php
@@ -51,14 +51,14 @@ use App\Core\Storage;
     <div class="col-12">
         <div class="row">
             <div class="col-md-4">
-                <div>
+                <div class="mt-5">
                     <h5>Profile Information</h5>
                     <small class="text-muted">Update your account's profile information.</small>
                 </div>
             </div>
             <div class="col-md-8">
                 <div class="row">
-                    <div class="col-12">
+                    <div class="col-12 mt-5">
                         <div class="card" style="background-color: #fff; border: 0px; border-radius: 8px; box-shadow: 0 4px 5px 0 rgba(0,0,0,0.2);">
                             <div class="card-body">
                                 <form method="POST" action="<?= route('/profile') ?>">
