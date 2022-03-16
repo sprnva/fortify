d6120c7ac3936b66d3b0dfc340b3fda76b919f39
Judywen Guapin <37282871+jagwarthegreat@users.noreply.github.com>
2022-03-16 11:43:34 +0800

    Update Fortify.php

--- src/Fortify.php
+++ src/Fortify.php
@@ -12,16 +12,16 @@ class Fortify
 
 		if (!file_exists('app/controllers/Auth')) {
 			// Controllers...
-			(new Filesystem)->ensureDirectoryExists(dirname(__DIR__, 4) . '/app/controllers/Auth');
-			(new Filesystem)->copyFortifyDirectory(__DIR__ . '/../stubs/default/app/controllers/Auth', dirname(__DIR__, 4) . '/app/controllers/Auth', '.php');
+			(new Filesystem)->ensureDirectoryExists(dirname(__DIR__) . '/../../../app/controllers/Auth');
+			(new Filesystem)->copyFortifyDirectory(__DIR__ . '/../stubs/default/app/controllers/Auth', dirname(__DIR__) . '/../../../app/controllers/Auth', '.php');
 		} else {
 			$exist++;
 		}
 
 		if (!file_exists('app/views/auth')) {
 			// Views...
-			(new Filesystem)->ensureDirectoryExists(dirname(__DIR__, 4) . '/app/views/auth');
-			(new Filesystem)->copyDirectory(__DIR__ . '/../stubs/default/app/views/auth', dirname(__DIR__, 4) . '/app/views/auth');
+			(new Filesystem)->ensureDirectoryExists(dirname(__DIR__) . '/../../../app/views/auth');
+			(new Filesystem)->copyDirectory(__DIR__ . '/../stubs/default/app/views/auth', dirname(__DIR__) . '/../../../app/views/auth');
 		} else {
 			$exist++;
 		}
